3981dafac8cf1d63359f017f983bfa12ce2d2ea0
aleaguirre11 <alexisaguirre100@gmail.com>
2022-11-01 18:15:42 -0300

    Clases

--- VideoClub/NLayer.Entidades/Copia.cs
+++ VideoClub/NLayer.Entidades/Copia.cs
@@ -6,7 +6,70 @@ using System.Threading.Tasks;
 
 namespace NLayer.Entidades
 {
-    internal class Copia
+    public class Copia : Pelicula
     {
+        public Copia(int idcopia, string observaciones, double precio, DateTime fechaalta, int idpelicula, int año, int duracion, string titulo, string director, string productora, string genero) : base(idpelicula, año, duracion, titulo, director, productora, genero)
+        {
+            _idcopia = idcopia;
+            _observaciones = observaciones;
+            _precio = precio;
+            _fechaalta = fechaalta;
+        }
+
+        private int _idcopia;
+        private string _observaciones;
+        private double _precio;
+        private DateTime _fechaalta;
+
+
+        public int Idcopia
+        {
+            get
+            {
+                return _idcopia;
+            }
+            set
+            {
+                _idcopia = value;
+            }
+        }
+
+        public string Observaciones
+        {
+            get
+            {
+                return _observaciones;
+            }
+            set
+            {
+                _observaciones = value;
+            }
+        }
+
+        public double Precio
+        {
+            get
+            {
+                return _precio;
+            }
+            set
+            {
+                _precio = value;
+            }
+        }
+
+        public DateTime Fechaalta
+        {
+            get
+            {
+                return _fechaalta;
+            }
+            set
+            {
+                _fechaalta = value;
+            }
+        }
     }
+
+   
 }
--- VideoClub/NLayer.Entidades/Pelicula.cs
+++ VideoClub/NLayer.Entidades/Pelicula.cs
@@ -6,7 +6,112 @@ using System.Threading.Tasks;
 
 namespace NLayer.Entidades
 {
-    public class Class1
+    public class Pelicula
     {
+        public Pelicula(int idpelicula, int año, int duracion, string titulo, string director, string productora, string genero)
+        {
+            _idpelicula = idpelicula;
+            _año = año;
+            _duracion = duracion;
+            _titulo = titulo;
+            _director = director;
+            _productora = productora;
+            _genero = genero;
+        }
+
+        private int _idpelicula;
+        private int _año;
+        private int _duracion;
+        private string _titulo;
+        private string _director;
+        private string _productora;
+        private string _genero;
+
+
+        public int IdPelicula
+        {
+            get
+            {
+                return _idpelicula;
+            }
+
+            set
+            {
+                _idpelicula = value;
+            }
+        }
+
+        public int Año
+        {
+            get
+            {
+                return _año;
+            }
+            set
+            {
+                _año = value;
+            }
+        }
+
+        public int Duracion
+        {
+            get
+            {
+                return _duracion;
+            }
+            set
+            {
+                _duracion = value;
+            }
+        }
+
+        public string Titulo
+        {
+            get
+            {
+                return _titulo;
+            }
+            set
+            {
+                _titulo = value;
+            }
+        }
+
+        public string Director
+        {
+            get
+            {
+                return _director;
+            }
+            set
+            {
+                _director = value;
+            }
+        }
+
+        public string Productora
+        {
+            get
+            {
+                return _productora;
+            }
+            set
+            {
+                _productora = value;
+            }
+        }
+
+        public string Genero
+        {
+            get
+            {
+                return _genero;
+            }
+            set
+            {
+                _genero = value;
+            }
+        }
+       
     }
 }
